26a6fe564af82cf80f062d0fd0202b75cf015e9e
Jack Polgar <hello@jp.id.au>
2022-07-18 20:40:03 +1000

    Update repo URL

--- src/Http/Routing/Route.php
+++ src/Http/Routing/Route.php
@@ -2,7 +2,7 @@
 /*
  * Lumos Framework
  * Copyright (c) 2022 Jack Polgar
- * https://gitlab.com/nirix/lumos
+ * https://gitlab.com/lumosphp/lumos
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
