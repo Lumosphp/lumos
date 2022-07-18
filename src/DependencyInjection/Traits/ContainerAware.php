3517e4c46bde658e4c14009474762f8706945e86
Jack Polgar <hello@jp.id.au>
2022-07-18 20:43:38 +1000

    Add `ContainerAware` trait

---
+++ src/DependencyInjection/Traits/ContainerAware.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Lumos Framework
+ * Copyright (c) 2022 Jack Polgar
+ * https://gitlab.com/lumosphp/lumos
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Lumos\DependencyInjection\Traits;
+
+use Lumos\DependencyInjection\ContainerInterface;
+
+trait ContainerAware
+{
+    protected ContainerInterface $container;
+
+    public function setContainer(ContainerInterface $container): void
+    {
+        $this->container = $container;
+    }
+}
